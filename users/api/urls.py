7501dc3d30730413239e50ca954362084ed5e764
muhammedatif <muhamedattif@gmail.com>
2022-03-10 12:56:05 +0200

    add Custom Auth token generator view

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserCreateView, AuthToken, StatusCreateView
+from .views import UserCreateView, AuthToken, StatusCreateView, EncrypedAuthToken
 app_name = 'users'
 
 urlpatterns = [
     path('', UserCreateView.as_view(), name='user-create'),
     path('auth-token', AuthToken.as_view(), name='auth-token'),
+    path('custom-auth-token', EncrypedAuthToken.as_view(), name='custom-auth-token'),
     path('status', StatusCreateView.as_view(), name='status-create')
 ]
--- users/api/views.py
+++ users/api/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth import get_user_model
 from users.models import Status
 from rest_framework.views import APIView
 from rest_framework import status
+import hashlib
+
 
 User = get_user_model()
 
@@ -30,7 +32,30 @@ class AuthToken(ObtainAuthToken):
         return Response({
             'token': token.key,
             'user_id': user.pk,
-            'phone_number': user.email
+            'phone_number': user.phone_number.raw_input
+        })
+
+# Generate encrypted Token based on phone number and password
+class EncrypedAuthToken(APIView):
+
+    permission_classes = []
+
+    def post(self, request):
+
+        serializer = AuthTokenSerializer(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=False)
+        user = serializer.validated_data['user']
+        phone_number = self.request.data['phone_number']
+        password = self.request.data['password']
+
+        plain_link = phone_number + password
+        generated_token = hashlib.md5(plain_link.encode('utf-8'))
+
+        return Response({
+            'token': generated_token.hexdigest(),
+            'user_id': user.pk,
+            'phone_number': user.phone_number.raw_input
         })
 
 
